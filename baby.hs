89609c353e787a203cd9ae4f47cac8f2423bbd57
Buntaro Orita <buntaro.orita@gmail.com>
2023-07-06 09:28:07 +0900

    pattern matching

--- baby.hs
+++ baby.hs
@@ -1,4 +1,5 @@
-{-# OPTIONS_GHC -Wall -Werror #-}
+-- {-# OPTIONS_GHC -Wall -Werror #-}
+{-# OPTIONS_GHC -Wall #-}
 
 doubleMe x = x + x
 
@@ -17,7 +18,9 @@ removeNonUppercase str =
     [c|c<-str, c `elem` ['A'..'Z']]
 
 factorial :: Integer -> Integer
-factorial n = product [1..n]
+-- factorial n = product [1..n]
+factorial 0 = 1
+factorial n = n * factorial (n-1)
 
 circumference :: Float -> Float
 circumference r = 2 * pi * r
@@ -28,3 +31,35 @@ circumference' r = 2 * pi * r
 lucky :: Int -> String
 lucky 7 = "Lucky number seven!"
 lucky x = "Sorry, you're out of luck, pal!"
+
+charName :: Char -> String
+charName 'a' = "Albert"
+charName 'b' = "Broseph"
+charName 'c' = "Cecil"
+charName _ = "???"
+
+addVectors :: (Double, Double) -> (Double, Double) -> (Double, Double)
+addVectors (x1, y1) (x2, y2) = (x1+x2, y1+y2)
+
+first :: (a,b,c) -> a
+first (a,_,_) = a
+
+second :: (a,b,c) -> b
+second (_,b,_) = b
+
+third :: (a,b,c) -> c
+third (_,_,c) = c
+
+head' :: [a] -> a
+head' [] = error "Can't call head on an empty list, dummy!"
+head' (x:_) = x
+
+tell :: (Show a) => [a] -> String
+tell [] = "The list is empty"
+tell (x:[]) = "The list has one element: " ++ show x
+tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y
+tell (x:y:_) = "This list is long. The first two elements are: " ++ show x ++ " and " ++ show y
+
+firstLetter :: String -> String
+firstLetter "" = "Empty string, whoops!"
+firstLetter all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
