760f3d6001b8ed8f860e55437ef430414dd8b3e5
Buntaro Orita <buntaro.orita@gmail.com>
2023-07-16 13:05:48 +0900

    where

--- baby.hs
+++ baby.hs
@@ -76,11 +76,13 @@ bmiTell bmi
 
 bmiTell' :: Double -> Double -> String
 bmiTell' weight height
-  | weight / height ^ 2 <= 18.5 = "You're underweight, you emo, you!"
-  | weight / height ^ 2 <= 25.0 = "You're supposedly normal.\
+  | bmi <= skinny = "You're underweight, you emo, you!"
+  | bmi <= normal = "You're supposedly normal.\
                                    \ Pffft, I bet you're ugly!"
-  | weight / height ^ 2 <= 30.0 = "You're fat! Lose some wight, fatty!"
+  | bmi <= fat    = "You're fat! Lose some wight, fatty!"
   | otherwise                   = "You're a whale, congratulations!"
+  where bmi = weight / height ^ 2
+        (skinny, normal, fat) = (18.5, 25.0, 30.0)
 
 max' :: (Ord a) => a -> a -> a
 max' a b
@@ -92,3 +94,22 @@ a `compare'` b
   | a == b    = EQ
   | a <= b    = LT
   | otherwise = GT
+
+
+niceGreeting :: String
+niceGreeting = "Hello! So very nice to see you,"
+badGreeting :: String
+badGreeting = "Oh! Pfft. It's you."
+greet :: String -> String
+greet "Juan" = niceGreeting ++ " Juan!"
+greet "Fernando" = niceGreeting ++ " Fernando!"
+greet name = badGreeting ++ " " ++ name
+
+initials :: String -> String -> String
+initials firstname lastname = [f] ++ ". " ++ [l]
+    where (f:_) = firstname
+          (l:_) = lastname
+
+calcBmis :: [(Double, Double)] -> [Double]
+calcBmis xs = [bmi w h |(w, h) <- xs]
+    where bmi weight height = weight / height ^ 2
